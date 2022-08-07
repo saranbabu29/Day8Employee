ff70c7c52ff5b5ee31ffb542ec8a5c019b845da9
saranbabu29 <sharanraj982@gmail.com>
2022-08-07 17:01:50 +0530

    Switch Case

--- Program.cs
+++ Program.cs
@@ -4,29 +4,29 @@ namespace EmployeeWage
 {
     class Program
     {
+        public const int isFullTime = 0;
+        public const int isPartTime = 1;
+        public const int WagePerHour = 20;
         static void Main(string[] args)
         {
-            int isPartTime = 1;
-            int wagePerHour = 20;
-            int empHr;
-            int dailyWage;
-
+            int emphr;
             Random random = new Random();
             int value = random.Next(0, 2);
 
-            if (value == isPartTime)
+
+            switch (value)
             {
-                empHr = 8;
-                Console.WriteLine("Employee is Full Time");
-                dailyWage = empHr * wagePerHour;
-                Console.WriteLine("Employee Wage is " + dailyWage);
-            }
-            else
-            {
-                empHr = 4;
-                Console.WriteLine("Employee is Part Time");
-                dailyWage = empHr * wagePerHour;
-                Console.WriteLine("Employee Wage is " +dailyWage);
+                case isFullTime:
+                    emphr = 8;
+                    Console.WriteLine("Employee is FullTime and Wage is " + (emphr * WagePerHour));
+                    break;
+                case isPartTime:
+                    emphr = 4;
+                    Console.WriteLine("Employee is PartTime and Wage is " + (emphr * WagePerHour));
+                    break;
+                default:
+                    emphr = 0;
+                    break;
             }
         }
     }
