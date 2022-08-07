2b5ada0c912d35e40f17a10f4e2e0651148d0175
saranbabu29 <sharanraj982@gmail.com>
2022-08-07 17:09:45 +0530

    Wage for Month

--- Program.cs
+++ Program.cs
@@ -7,9 +7,11 @@ namespace EmployeeWage
         public const int isFullTime = 0;
         public const int isPartTime = 1;
         public const int WagePerHour = 20;
+        public const int days = 20;
         static void Main(string[] args)
         {
             int emphr;
+            int wage;
             Random random = new Random();
             int value = random.Next(0, 2);
 
@@ -18,11 +20,13 @@ namespace EmployeeWage
             {
                 case isFullTime:
                     emphr = 8;
-                    Console.WriteLine("Employee is FullTime and Wage is " + (emphr * WagePerHour));
+                    wage = emphr * WagePerHour;
+                    Console.WriteLine("Employee is FullTime and Wage for Month is " + (emphr * wage));
                     break;
                 case isPartTime:
                     emphr = 4;
-                    Console.WriteLine("Employee is PartTime and Wage is " + (emphr * WagePerHour));
+                    wage = emphr * WagePerHour;
+                    Console.WriteLine("Employee is PartTime and Wage for Month is " + (emphr * wage));
                     break;
                 default:
                     emphr = 0;
